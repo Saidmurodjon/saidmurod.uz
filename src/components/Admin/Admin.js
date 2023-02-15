0c8023a9ba4d128b844232d12c1e07a86b6fee5e
saidmurod <saifdmurodjon1020@gmail.com>
2023-02-15 15:06:26 +0500

    admin/login

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,5 +1,5 @@
 const Admin = () => {
-  window.location.href = process.env.REACT_APP_SERVER_URL;
+  window.location.href = process.env.REACT_APP_SERVER_URL+"admin/login";
 };
 
 export default Admin;
